8936964f622b48260b43f3ab5c5f22ef214fe2ac
DS0708 <jonum12312@gmail.com>
2025-01-28 22:03:10 +0900

    인프라 관련 설정 변경

--- infra/kOps/kops_init.sh
+++ infra/kOps/kops_init.sh
@@ -1,65 +1,39 @@
+# kOps를 위한 S3 버킷 생성 (ap-northeast-2 리전)
 aws s3api create-bucket --bucket zipbob-k8s-bucket --create-bucket-configuration LocationConstraint=ap-northeast-2
 
+# S3 버킷의 버전 관리 활성화
 aws s3api put-bucket-versioning --bucket zipbob-k8s-bucket \
 --versioning-configuration Status=Enabled
 
+# 클러스터 이름과 상태 저장소 설정
 export NAME=zipbob.cluster.k8s
 export KOPS_STATE_STORE=s3://zipbob-k8s-bucket
 
+# 클러스터 생성
 kops create cluster \
 --zones ap-northeast-2a \
 --networking calico \
 --ssh-public-key ./id_rsa.pub $NAME \
 --node-count=5 \
---control-plane-size=t3.medium \
---node-size=t3.small \
---control-plane-volume-size=50 \
---node-volume-size=30
+--control-plane-size=t3.xlarge \
+--node-size=c5.large
 
+# 클러스터 생성 실행
 kops update cluster --yes $NAME
 
+# kubeconfig 설정
 kops export kubeconfig --admin
 
-
-aws iam attach-role-policy --role-name masters.zipbob.cluster.k8s --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy
-aws iam attach-role-policy --role-name nodes.zipbob.cluster.k8s --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy
-
-
-
-# image
-# ECR login
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com
-# image tag
-docker tag zipbob-config-service:latest 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-config-service:latest
-docker tag zipbob-edge-service:latest 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-edge-service:latest
-docker tag ingredients-manage-service:latest 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/ingredients-manage-service:latest
-docker tag recipe-review-service:latest 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/recipe-review-service:latest
-# 이미지 푸시 (다시 시도)
-docker push 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-config-service:latest
-docker push 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-edge-service:latest
-docker push 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/ingredients-manage-service:latest
-docker push 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/recipe-review-service:latest
-# image name list
-905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-config-service:latest
-905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-edge-service:latest
-905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/ingredients-manage-service:latest
-905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/recipe-review-service:latest
-
-# zipbob-llm, ECR 생성 먼저 해야함.
-aws ecr create-repository --repository-name zipbob-llm --region ap-northeast-2
-docker tag jonum12312/zipbob-llm:0.0.1 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-llm:0.0.1
-docker push 905418374604.dkr.ecr.ap-northeast-2.amazonaws.com/zipbob-llm:0.0.1
-
-#EBS 관련 IAM
-aws iam attach-role-policy --role-name masters.zipbob.cluster.k8s --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy
-aws iam attach-role-policy --role-name nodes.zipbob.cluster.k8s --policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy
 
 
 ##
 ## kops 설정 변경
 ##
+# 인스턴스 그룹 조회
 kops get ig
+# 컨트롤 플레인 인스턴스 그룹 설정 수정
 kops edit ig control-plane-ap-northeast-2a 
+# 워커 노드 인스턴스 그룹 설정 수정
 kops edit ig nodes-ap-northeast-2a
 # 변경사항 적용
 kops update cluster --yes
@@ -69,7 +43,5 @@ kops rolling-update cluster --yes
 
 
 
-
-
-# delete cluster
+# 클러스터 삭제
 kops delete cluster --name=$NAME --yes --state=$KOPS_STATE_STORE
